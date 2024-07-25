34278ef34d247a982d05c731c39ebf64c2cdc30b
user <afanasy.ananin@yandex.ru>
2024-07-25 15:05:12 +0300

    test for github1 add method print

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,5 +1,11 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("first test from work");
+        System.out.println("first test from work2");
+        System.out.println("first test from work3");
+        print();
+    }
+    public static void print() {
+        System.out.println("11");
     }
 }
