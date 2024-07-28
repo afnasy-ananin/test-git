135f42a4ef8c1a5d67515524e815f60d8d3767a3
afnasy-ananin <131880080+afnasy-ananin@users.noreply.github.com>
2024-07-28 15:21:06 +0300

    Update TestGit.java

--- src/TestGit.java
+++ src/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
         String str = "add this str and try merge and pull";
         String str = "New from home";
         String str = "New from home, second corrected";
+
     }
 }
